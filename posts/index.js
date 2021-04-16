37bc4ec8848fe62c0dc5c10a86019b5f21cee909
iqbalrafat <iqbalrafat@yahoo.com>
2021-04-16 15:03:20 -0600

    [microservices]-- created posts component in react

--- posts/index.js
+++ posts/index.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const {randomBytes} = require ("crypto");
-//we use the object to take care of data as we are not using data base. 
-const posts ={};
+const { randomBytes } = require("crypto");
+//we use the object to take care of data as we are not using data base.
+const posts = {};
 const app = express();
-app.use (bodyParser.json())
+app.use(bodyParser.json());
 app.post("/posts", (req, res) => {
   //we need to give ID our post for that we use randomBytes.
-  const id = randomBytes(4).toString('hex');
-  const {title}=req.body;
-  posts[id]= {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+  posts[id] = {
     id,
-    title
+    title,
   };
   //after creating Posts we inform rest of the services that post is created so send the data
   res.status(201).send(posts[id]);
 });
 app.get("/posts", (req, res) => {
-  res.send(posts)
+  res.send(posts);
 });
 
 app.listen(4000, () => {
