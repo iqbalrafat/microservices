e9054810ee6d7f6c457f75e01207ba78efd94ca3
iqbalrafat <iqbalrafat@yahoo.com>
2021-04-22 23:58:28 -0600

    [microservices]--update query service

--- query/index.js
+++ query/index.js
@@ -7,11 +7,31 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/posts',(req,res)=>{
+  res.send(posts);
 
 });
 app.post('/events',(req,res)=>{
+  //when post or comment created an event that event has type. now we use that type to further processing.
+  //post or comment create event, event-bus receives it and send back to all services. 
+  const {type,data}=req.body;
+  if (type==="PostCreated"){
+    //when we created the post it has type and data. now from data we can take out title and id.
+    const {id,title}=data;
 
+    post[id]={id,title,comments:[]};  //we added comments for future posts
+  }
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  console.log(posts);
+
+  res.send({});
 });
+
 
 app.listen(4002,(req,res)=>{
   console.log("server running at 4002")
