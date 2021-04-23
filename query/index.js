ffab9716ef0eb8949127dd02bce629a6e7885726
iqbalrafat <iqbalrafat@yahoo.com>
2021-04-22 23:37:37 -0600

    [microservices]-- added query services

---
+++ query/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors =require('cors');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/posts',(req,res)=>{
+
+});
+app.post('/events',(req,res)=>{
+
+});
+
+app.listen(4002,(req,res)=>{
+  console.log("server running at 4002")
+});
